c490d13d2ecd88a6fdad41b4aa586df389ff5254
Linus <lkurz@posteo.de>
2026-02-06 02:47:10 +0100

    Add Utility Functions

--- src/Util.cpp
+++ src/Util.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Util.h"
+
+#include "ArduinoLog.h"
+
+std::string Util::logLevelToString(int logLevel) {
+    switch (logLevel) {
+        default:
+        case 0: return "SILENT ";
+        case 1: return "FATAL ";
+        case 2: return "ERROR ";
+        case 3: return "WARNING ";
+        case 4: return "INFO ";
+        case 5: return "TRACE ";
+        case 6: return "VERBOSE ";
+    }
+}
+
+
+void Util::printDMXData(const uint8_t *data, uint16_t size) {
+    Log.verboseln("DMX Data: |");
+    for (uint16_t i = 0; i < size; ++i) {
+        Log.verbose("%d", data[i]);
+        if (i < size - 1) {
+            Log.verbose("|");
+        }
+    }
+    Log.verboseln("|");
+}
